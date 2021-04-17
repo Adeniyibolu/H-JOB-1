429b11b60fb25ab67b56d8c20dc894c965ccf010
ADEBAYO SAMUEL OLUWASEYI <adebayosamueljahsmine30@gmail.com>
2021-04-17 09:17:34 +0100

    separated FE and BE files

---
+++ Client/build/precache-manifest.a19c95d5cdb0f709231f523794ed5105.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50f39dab3248e66dfb4d751a5cc54f87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15afa48f11dde7ec218b",
+    "url": "/static/css/main.fb6e3d98.chunk.css"
+  },
+  {
+    "revision": "6a6c3aad92c2a718314b",
+    "url": "/static/js/2.59a200a0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.59a200a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15afa48f11dde7ec218b",
+    "url": "/static/js/main.8fa325cd.chunk.js"
+  },
+  {
+    "revision": "c3655119f17e286e58a8",
+    "url": "/static/js/runtime-main.dbfcc0d1.js"
+  }
+]);
